37900d4daff4fbb0e707682d076bfdc6f5954500
Evanflow89 <ivanbaragone@gmail.com>
2022-06-10 11:18:35 +0200

    bug fixed

--- src/components/commons/SearchBar.vue
+++ src/components/commons/SearchBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="form">
-    <form>
+    <form @submit.prevent="searching">
       <input type="text" v-model="search.search" />
       <button>Submit</button>
     </form>
@@ -9,12 +9,33 @@
 
 <script>
 import search from "../../shared/search";
+import data from "../../shared/data.js";
+import axios from "axios";
 export default {
   name: "SearchBar",
   data() {
     return {
       search,
+      data,
     };
+  },
+  methods: {
+    searching() {
+      axios
+        .get("https://api.themoviedb.org/3/search/movie", {
+          params: {
+            api_key: "a7f35c47cf7e3c0ea7a66c66aec0b919",
+            query: this.search.search,
+            language: "it-IT",
+          },
+        })
+        .then((response) => {
+          data.films = response.data.results;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
 };
 </script>
--- src/components/sections/SectionsResults.vue
+++ src/components/sections/SectionsResults.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-12 text-center">
         <ul>
-          <li v-for="film in films" :key="film.id">
+          <li v-for="film in data.films" :key="film.id">
             <span>Titolo del Film : {{ film.title }}</span>
             <span>Titolo Originale : {{ film.original_title }}</span>
             <span>Lingua : {{ film.original_language }}</span>
@@ -16,32 +16,16 @@
 </template>
 
 <script>
-import axios from "axios";
 import search from "../../shared/search.js";
+import data from "../../shared/data.js";
 
 export default {
   name: "SectionsResults",
   data() {
     return {
       search,
-      films: [],
+      data,
     };
-  },
-  created() {
-    axios
-      .get("https://api.themoviedb.org/3/search/movie", {
-        params: {
-          api_key: "a7f35c47cf7e3c0ea7a66c66aec0b919",
-          query: "prova",
-          language: "it-IT",
-        },
-      })
-      .then((response) => {
-        this.films = response.data.results;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
   },
 };
 </script>
