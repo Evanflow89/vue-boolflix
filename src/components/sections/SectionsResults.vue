14d8923ced616c418b262780da4fbe26b158d605
Evanflow89 <ivanbaragone@gmail.com>
2022-06-11 09:30:36 +0200

    Added series section

--- src/components/sections/SectionsResults.vue
+++ src/components/sections/SectionsResults.vue
@@ -1,17 +1,35 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-12 text-center">
-        <ul>
-          <li v-for="film in data.films" :key="film.id">
-            <span>Titolo del Film : {{ film.title }}</span>
-            <span>Titolo Originale : {{ film.original_title }}</span>
-            <span>Lingua : {{ film.original_language }}</span>
-            <span>Voto Medio :{{ film.vote_average }}</span>
-          </li>
-        </ul>
+    <section class="Movies">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h2>Movies</h2>
+          <ul>
+            <li v-for="film in data.films" :key="film.id">
+              <span>Titolo del Film : {{ film.title }}</span>
+              <span>Titolo Originale : {{ film.original_title }}</span>
+              <span>Lingua : {{ film.original_language }}</span>
+              <span>Voto Medio :{{ film.vote_average }}</span>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </section>
+    <section class="Series">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h2>Series</h2>
+          <ul>
+            <li v-for="serie in data.series" :key="serie.id">
+              <span>Titolo della Serie : {{ serie.name }}</span>
+              <span>Titolo Originale : {{ serie.original_name }}</span>
+              <span>Lingua : {{ serie.original_language }}</span>
+              <span>Voto Medio :{{ serie.vote_average }}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
